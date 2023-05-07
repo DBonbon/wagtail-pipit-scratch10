83711c12e8292b09f0818bf7966ba993aacc9b72
dbonbon <yrfriedman@gmail.com>
2023-05-08 00:02:09 +0200

    postgis

--- pipit/settings/base.py
+++ pipit/settings/base.py
@@ -262,10 +262,10 @@ WAGTAIL_HEADLESS_PREVIEW = {
 SENTRY_DSN: Optional[str] = None
 SENTRY_ENVIRONMENT: Optional[str] = None
 
-if os.name == 'nt':
+"""if os.name == 'nt':
     VENV_BASE = os.environ['VIRTUAL_ENV']
     os.environ['PATH'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
     os.environ['PROJ_LIB'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo\\data\\proj') + ';' + os.environ['PATH']
-
+"""
 
 CORS_ORIGIN_ALLOW_ALL = True
