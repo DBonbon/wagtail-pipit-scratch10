5099278b1ee055dd6809c7ff0b96998af367b91b
dbonbon <yrfriedman@gmail.com>
2023-05-08 12:09:13 +0200

    remove gist

--- pipit/settings/base.py
+++ pipit/settings/base.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sitemaps",
-    "django.contrib.gis",
+    #"django.contrib.gis",
     # Third party apps
     "wagtail.embeds",
     "wagtail.sites",
